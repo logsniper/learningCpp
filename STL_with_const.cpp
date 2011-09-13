4c52cb8a3bdd3790cae6adcb4b4fb04a77b11963
Hao Xie <xiehao0221@gmail.com>
2011-09-13 20:05:10 +0800

    by xiehao

---
+++ STL_with_const.cpp
@@ -0,0 +1,15 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int main(){
+	vector<int> array;
+	array.push_back(1);
+	array.push_back(2);
+	array.push_back(3);
+//	const
+	vector<int> &ref = array;
+	for(vector<int>::iterator it=ref.begin();it!=ref.end();++it){
+		cout<<*it<<endl;
+	}
+}
